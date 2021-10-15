32d2d3594b1be18d04a2e9721aba9c56b3ceed3b
Paolo Giua <paolo.giuaa@gmail.com>
2021-10-15 13:04:19 +0200

    Fixed the cache problem

--- app/js/request.js
+++ app/js/request.js
@@ -1,8 +1,22 @@
 function makeRequest(url, callback, error) {
-    $.get(url, (data) => {
+    /* $.get(url, (data) => {
         callback(data, url);
     }, "text").fail(function(e) {
         if(error)
             error(e);
-    });;
+    }); */
+    
+    $.ajax({
+        url: url,
+        method: "GET",
+        dataType: "text",
+        cache: false,
+        success: (data) => {
+            callback(data, url);
+        },
+        error: (e) => {
+            if(error)
+                error(e);
+        }
+    });
 }
