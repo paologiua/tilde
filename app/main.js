4834d2625536d79cb8c020147e846e5a3a0716ab
Paolo Giua <paolo.giuaa@gmail.com>
2021-10-28 22:09:33 +0200

    Added fast window loading

--- app/main.js
+++ app/main.js
@@ -1,24 +1,24 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-const url = require('url')
-/* 
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+ 
 // Import getPreference() helper function
-const getPreference = require('./js/helper/helper_global')
- */
+const getPreference = require('./js/helper/helper_global');
+/*  
 // Modules to create app tray icon
 const Menu = require('electron').Menu
 const Tray = require('electron').Tray
-
+ */
 // Create variables for appIcon, trayIcon, win
 // to prevent their removal by garbage collection
-let appIcon = null
-let trayIcon = null
-let win
+/* let appIcon = null */
+let trayIcon = null;
+let win = null;
 
 // Request lock to allow only one instance
 // of the app running at the time.
-const gotTheLock = app.requestSingleInstanceLock()
-
+const gotTheLock = app.requestSingleInstanceLock();
+/* 
 // Load proper icon for specific platform
 if (process.platform === 'darwin') {
     trayIcon = path.join(__dirname, './img/tilde16x16.png')
@@ -27,9 +27,21 @@ if (process.platform === 'darwin') {
 } else if (process.platform === 'win32') {
     trayIcon = path.join(__dirname, './img/tilde.ico')
 }
+ */
+// Load proper icon for specific platform
+switch (process.platform) {
+    case 'darwin':
+        trayIcon = path.join(__dirname, './img/tilde16x16.png');
+        break;
+    case 'linux':
+        trayIcon = path.join(__dirname, './img/tilde.png');
+        break;
+    case 'win32':
+        trayIcon = path.join(__dirname, './img/tilde.ico');
+        break;
+}
 
-function createWindow()
-{
+function createWindow() {
 	win = new BrowserWindow({
         width: 1100,
         minWidth: 1000,
@@ -42,15 +54,20 @@ function createWindow()
             nodeIntegration: true,
             enableRemoteModule: true,
             zoomFactor: 0.9
-        }
+        },
+        show: false
     });
     
-    win.loadURL(url.format
-    ({
+    win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashed: true
-    }))
+    }));
+
+    win.setBackgroundColor(getPreference('darkmode') ? '#333' : '#fff');
+    win.webContents.on('did-finish-load', function() {
+        win.show();
+    });
 /* 
     // Create tray icon
     appIcon = new Tray(trayIcon)
@@ -93,8 +110,8 @@ function createWindow()
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        win = null
-    })
+        win = null;
+    });
 /* 
     // Minimize window to system tray if 'Minimize' option is checked
     if (getPreference('minimize') === true) {
@@ -106,15 +123,13 @@ function createWindow()
     }
  */
     // Quit when all windows are closed
-    win.on('window-all-closed', () =>
-    {
-        app.quit()
-    })
+    win.on('window-all-closed', () => {
+        app.quit();
+    });
 
-    win.on('closed', () =>
-    {
-        app.quit()
-    })
+    win.on('closed', () => {
+        app.quit();
+    });
 
 /*  // FUNCTIONS TO COMMUNICATE WITH THE RENDER
     function sendToRender(channel, obj) {
@@ -133,17 +148,18 @@ function createWindow()
 
 // Check if this is first instance of the app running.
 // If not, block it. If yes, allow it.
-if (!gotTheLock) {
-    app.quit()
+if(!gotTheLock) {
+    app.quit();
 } else {
     app.on('second-instance', (event, commandLine, workingDirectory) => {
         // Someone tried to run a second instance, we should focus our window.
-        if (win) {
-            if (win.isMinimized()) win.restore()
-                win.focus()
+        if(win) {
+            if(win.isMinimized()) 
+                win.restore();
+            win.focus();
         }
     })
 
     // Create win, load the rest of the app, etc...
-    app.on('ready', createWindow)
+    app.on('ready', createWindow);
 }
